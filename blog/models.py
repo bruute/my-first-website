846b74d3564dd6d93c6b705866aed8b50c6b118c
Yakov <leonovyakov@gmail.com>
2015-11-18 12:42:23 -0500

    lets see if this works

--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,6 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-    class Meta:
-        ordering = ['-date',]
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
+   class Meta:
+        odering = ['-date',]
