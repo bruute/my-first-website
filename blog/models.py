f7b0dc06b9d752fa27396f8e4c9ff7aaf2d45608
Yakov <leonovyakov@gmail.com>
2015-11-18 12:37:44 -0500

    backup of original models.py

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
-class Blog(models.Model):
-    title = models.CharField(max_length=32)
-    date = models.DateTimeField(auto_now_add=True)
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
     text = models.TextField()
-
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
     class Meta:
         ordering = ['-date',]
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
